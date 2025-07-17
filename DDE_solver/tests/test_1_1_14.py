c71fc38ffa001e243b7044e79c50d3b5cae5b30c
kaique <kaiquemicaelmoreiradeoliveira@gmail.com>
2025-07-17 19:25:20 -0300

    bunch of more stuff

---
+++ DDE_solver/tests/test_1_1_14.py
@@ -0,0 +1,37 @@
+from DDE_solver.rkh_state import *
+
+# WARN: STATE EXAMPLE
+
+
+def f(t, y, yq):
+    A = 1 - np.exp(-3*np.pi/2)
+    return A * y + yq - A * np.sin(t)
+
+
+def phi(t):
+    return np.exp(t) + np.sin(t)
+
+
+def alpha(t, y):
+    return t - 3*np.pi/2
+
+
+def real_sol(t):
+    return np.exp(t) + np.sin(t)
+
+
+t_span = [1, 5]
+
+solver = Solver(f, alpha, phi, t_span)
+solver.solve_dde()
+tt = np.linspace(t_span[0], t_span[1], 100)
+realsol = np.array([real_sol(t) for t in tt])
+sol = np.array([solver.eta(i) for i in tt])
+# for i in range(len(tt)):
+#     print(tt[i], realsol[i] - sol[i])
+print("max", max(abs(sol - realsol)))
+
+
+plt.plot(tt, realsol, color="red")
+plt.plot(tt, sol, color="blue")
+plt.show()
